933757a94ece01205b5d0824b3bd9f4bfac1903f
Changbeom Ahn <kewlbear@gmail.com>
2021-11-11 09:15:08 +0900

    fix build error

---
+++ ios/patch.sh
@@ -0,0 +1,11 @@
+cd 3rdparty/pybind11
+git apply ../0001-fix-for-iOS.patch
+cd -
+
+cd 3rdparty/libjpeg-turbo/libjpeg-turbo
+git apply ../0001-fix-for-iOS.patch
+cd -
+
+cd 3rdparty/PoissonRecon/PoissonRecon
+git apply ../0001-fix-for-iOS.patch
+cd -
